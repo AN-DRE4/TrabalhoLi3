4ca87f1d19bc3457ce2727673117a604fe67b4ea
André Alves <73426983+AN-DRE4@users.noreply.github.com>
2022-11-09 22:30:41 +0000

    Add files via upload
    Ficheiro com diferentes defines utilizados em todo o projeto

---
+++ src/define.h
@@ -0,0 +1,20 @@
+/**
+ * @file define.h
+ * @brief Header com diferentes defines utilizados em todo o projeto
+ */
+
+#ifndef DEFINE_H
+#define DEFINE_H
+
+#define DRIVERS_FILEPATH "entrada/drivers.csv"
+#define RIDES_FILEPATH "entrada/rides.csv"
+#define USERS_FILEPATH "entrada/users.csv"
+
+#define LAST_DATE "09/10/2022"
+#define LAST_DATE_DIA 9
+#define LAST_DATE_MES 10
+#define LAST_DATE_ANO 2022
+#define LINE_BUFFER 1000000
+#define TABLE_SIZE 5000000
+
+#endif
