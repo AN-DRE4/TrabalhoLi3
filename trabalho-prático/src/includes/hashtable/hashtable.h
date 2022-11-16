db7ee4869ed79563de91bab2b1e1d2fff8625f90
André Alves <73426983+AN-DRE4@users.noreply.github.com>
2022-11-16 10:07:56 +0000

    Update hashtable.h

--- trabalho-prático/src/includes/hashtable/hashtable.h
+++ trabalho-prático/src/includes/hashtable/hashtable.h
@@ -19,6 +19,7 @@ void ht_insert_3(ht *ht, char *key, void *d);
 
 void* ht_get(ht *ht, char *key, void*(*create_type)(void),void(*copy_data)(void*, void*));
 void* ht_get_no_mem_cpy(ht *ht, char *key);
+void* ht_get_next(ht *ht, char *key, int r);
 char* ht_get_s(ht *ht, int *slot, int *node, void** d);
 
 int ht_count(ht *ht);
