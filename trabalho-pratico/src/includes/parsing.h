4679ff82c2092e7e12d63448b5f1ebeb32e81d49
André Alves <73426983+AN-DRE4@users.noreply.github.com>
2022-11-21 12:12:44 +0000

    Update parsing.h

--- trabalho-pratico/src/includes/parsing.h
+++ trabalho-pratico/src/includes/parsing.h
@@ -6,14 +6,14 @@
 #ifndef PARSING_H
 #define PARSING_H
 
-#include "modules/users/user.h"
-#include "modules/drivers/driver.h"
-#include "modules/rides/ride.h"
+#include "modules/users/users.h"
+#include "modules/drivers/drivers.h"
+#include "modules/rides/rides.h"
 
 #include "hashtable/hashtable.h"
 
-USER create_users_catalog();
-DRIVER create_drivers_catalog();
-RIDE create_rides_catalog(USER us, DRIVER ds, ht *ht_user_ride, ht *ht_driver_ride);
+USERS create_users_catalog();
+DRIVERS create_drivers_catalog();
+RIDES create_rides_catalog(USERS us, DRIVERS ds, ht *ht_user_ride, ht *ht_driver_ride);
 
 #endif
